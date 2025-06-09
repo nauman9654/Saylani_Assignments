2c9dbbf90a4577a8df4fd53bccb02d55ea62a78d
nauman9654 <thomasedison937@gmail.com>
2025-06-09 11:51:56 +0500

    Added assignment-1: Answer 1,2,3,4,5,6 of chap1

---
+++ assignments/assignment-1/index.js
@@ -0,0 +1,18 @@
+//ans1
+alert("Welcome to Progrmming World...");
+//ans2
+alert("Error! Please enter a valid password");
+//ans3
+alert("Welcome to JS Land...\nHappy Coding!");
+
+
+//ans4
+alert("Welcome to JS Land...");
+alert("Happy Coding!");
+
+
+//ans5
+console.log("Hello... I can run JS through my web browser's console")
+
+//ans6
+alert("Practicing alert")
